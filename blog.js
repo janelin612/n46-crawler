ebbc92fba3cff3543f889925b5064e11e143721b
Jian-lin Wu <wgl1242254@gmail.com>
2017-12-30 14:50:35 +0800

    爬蟲新增爬取作者

--- blog.js
+++ blog.js
@@ -70,6 +70,9 @@ var archieveCrawler=new Crawler({
               }
             );
             pageCountCrawler.queue(archieveList);
+
+            var member=$("#sideprofile .txt p a").attr("href");
+            memberDetailCrawler.queue(member);
         }
         done();
     }
@@ -149,6 +152,59 @@ var blogCrawler= new Crawler({
         }
         done();
     }
+});
+
+var member={};
+var memberDetailCrawler=new Crawler({
+  maxConnections : 1,
+  callback : function (error, res, done) {
+      if(error){
+          console.log(error);
+      }else{
+          var URL=res.request.uri.href;
+          var $ = res.$;
+
+          var memberImage=$("#profile img").first().attr("src");
+
+          if(downloadImage){
+            Image.downloader.queue(memberImage);
+            memberImage="img/"+memberImage.replace(/^http\S\/\/\S+?\//,'');
+          }
+
+          var memberName_hiragana=$("#profile div.txt h2 span").text();
+          $("#profile div.txt h2 span").remove();
+          var memberName=$("#profile div.txt h2").text();
+
+          //table
+          var introList=[];
+          $("#profile div.txt dl dt").each(function(index,view){
+            var key=$(view).text().replace("：","");
+            var value=$(view).nextAll("dd").first().text();
+            introList.push({
+              key:key,value:value
+            });
+          });
+
+          //tag
+          var tagList=[];
+          $("#profile div.txt div.status div").each(function(index,view){
+            tagList.push($(view).text());
+          });
+
+          //write all
+          member={
+            name:memberName,
+            name_hiragana:memberName_hiragana,
+            image:memberImage,
+            intro:introList,
+            tag:tagList
+          }
+
+          var fileName='./viewer/member.json'
+          fs.writeFile(fileName, JSON.stringify(member), 'utf8');
+      }
+      done();
+  }
 });
 
 //執行!
