1f9d37437f440716fceeefd9917e575fc2269fd8
Jian-lin Wu <wgl1242254@gmail.com>
2017-12-31 12:26:27 +0800

    將取得成員介紹的程式碼拆分出去

--- blog.js
+++ blog.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Crawler = require("crawler");
 const Image=require('./image')
+const Member=require('./member')
 
 /**
  * 主網站網址
@@ -71,8 +72,9 @@ var archieveCrawler=new Crawler({
             );
             pageCountCrawler.queue(archieveList);
 
-            var member=$("#sideprofile .txt p a").attr("href");
-            memberDetailCrawler.queue(member);
+            var memberUrl=$("#sideprofile .txt p a").attr("href");
+            Member.downloadImage=this.downloadImage;
+            Member.crawler.queue(memberUrl);
         }
         done();
     }
@@ -156,58 +158,7 @@ var blogCrawler= new Crawler({
     }
 });
 
-var member={};
-var memberDetailCrawler=new Crawler({
-  maxConnections : 1,
-  callback : function (error, res, done) {
-      if(error){
-          console.log(error);
-      }else{
-          var URL=res.request.uri.href;
-          var $ = res.$;
 
-          var memberImage=$("#profile img").first().attr("src");
-
-          if(downloadImage){
-            Image.downloader.queue(memberImage);
-            memberImage="img/"+memberImage.replace(/^http\S\/\/\S+?\//,'');
-          }
-
-          var memberName_hiragana=$("#profile div.txt h2 span").text();
-          $("#profile div.txt h2 span").remove();
-          var memberName=$("#profile div.txt h2").text();
-
-          //table
-          var introList=[];
-          $("#profile div.txt dl dt").each(function(index,view){
-            var key=$(view).text().replace("：","");
-            var value=$(view).nextAll("dd").first().text();
-            introList.push({
-              key:key,value:value
-            });
-          });
-
-          //tag
-          var tagList=[];
-          $("#profile div.txt div.status div").each(function(index,view){
-            tagList.push($(view).text());
-          });
-
-          //write all
-          member={
-            name:memberName,
-            name_hiragana:memberName_hiragana,
-            image:memberImage,
-            intro:introList,
-            tag:tagList
-          }
-
-          var fileName='./viewer/member.json'
-          fs.writeFile(fileName, JSON.stringify(member), 'utf8');
-      }
-      done();
-  }
-});
 
 //執行!
 if(argv.list){
