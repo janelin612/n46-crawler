526446e4abd6f34163340e4eace01c8590200cad
Jian-lin Wu <wgl1242254@gmail.com>
2019-03-31 21:32:59 +0800

    rebase all commit in 2019

--- script/main.js
+++ script/main.js
@@ -1,78 +1,104 @@
+"use strict";
+
 var app = new Vue({
-    el: "#app",
-    data: {
-        weekday:['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
-        firstTimeUpdate:true,
-        selected: 0,
-        list: [],
-        member: {},
-        isFold:false
+  el: "#app",
+  data: {
+    weekday: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    selected: 0,
+    list: [],
+    member: {},
+    isFold: false,
+    isLoading: true
+  },
+  computed: {
+    //指向目前選定的部落格文章
+    currentPost: function currentPost() {
+      if (this.list.length == 0 || this.selected < 0 || this.selected >= this.list.length) {
+        return null;
+      } else {
+        return this.list[this.selected];
+      }
     },
-    computed:{
-        //指向目前選定的部落格文章
-        currentPost:function(){
-            if(this.list.length==0 || this.selected<0 ||this.selected>=this.list.length ){
-                return null;
-            }else{
-                return this.list[this.selected];
-            }
-        },
-        //由date計算出來實際要顯示的日期樣式物件
-        currentPostDate:function(){
-            if(this.currentPost==null){
-                return {
-                    yearMonth:"",
-                    date:"",
-                    weekday:""
-                }
-            }else{
-                var time = new Date(this.currentPost.datetime);
-                var month=(time.getMonth()+1)>9?""+(time.getMonth()+1):"0"+(time.getMonth()+1);
-                var date=time.getDate()>9?""+time.getDate():"0"+time.getDate();
-                return {
-                    yearMonth:time.getFullYear()+"/"+month,
-                    date:date,
-                    weekday:this.weekday[time.getDay()]
-                }
-            }
-        }
-    },
-    methods: {
-        onListSelected: function (index) {
-            window.scrollTo(0, 0);
-            this.isFold=true;
-            this.selected = index;
-            var obj = { Url: window.location.toString().split("?")[0] + "?no=" + index };
-            history.replaceState(obj, document.title, obj.Url);
-        },
-        onToggle:function(){
-            this.isFold=!this.isFold;
-        }
-    },
-    created: function () {
-        this.$http.get("./result.json").then(res => { this.list = res.body }, res => { this.list = [] });
-        this.$http.get("./member.json").then(res => { this.member = res.body }, res => { this.member = {} });
-
-        var no = new URL(window.location).searchParams.get("no");
-        if (no != null && no != '') {
-            this.selected = no;
-        }
-    },
-    updated:function(){
-        
-        if(this.firstTimeUpdate){
-            //更改網站標題
-            if('name' in this.member){
-                document.title=this.member.name+" 非公式ブログ";
-            }
-
-            //第一次開啟時將卷軸捲到正確的位置
-            var dom=this.$el.querySelector(".menu li");
-            if(dom!=null){
-                var single=dom.scrollHeight;
-                this.$el.querySelector(".menu").scrollTop=single*(this.selected);
-                this.firstTimeUpdate=false;
-            }
-        }
+    //由date計算出來實際要顯示的日期樣式物件
+    currentPostDate: function currentPostDate() {
+      if (this.currentPost == null) {
+        return {
+          yearMonth: "",
+          date: "",
+          weekday: ""
+        };
+      } else {
+        var time = new Date(this.currentPost.datetime);
+        var month = this.formatNumber(time.getMonth() + 1);
+        var date = this.formatNumber(time.getDate());
+        return {
+          yearMonth: time.getFullYear() + "/" + month,
+          date: date,
+          weekday: this.weekday[time.getDay()]
+        };
+      }
     }
+  },
+  methods: {
+    onListSelected: function onListSelected(index) {
+      window.scrollTo(0, 0);
+      this.isFold = true;
+      this.selected = index;
+      var obj = {
+        Url: window.location.toString().split("?")[0] + "?no=" + index
+      };
+      history.replaceState(obj, document.title, obj.Url);
+    },
+    onToggle: function onToggle() {
+      this.isFold = !this.isFold;
+    },
+
+    /** 一位數字前面補零 */
+    formatNumber: function formatNumber(num) {
+      return num > 9 ? `${num}` : `0${num}`;
+    }
+  },
+  created: function created() {
+    var _this = this;
+
+    if (typeof defaultList !== 'undefined') {
+      this.list = defaultList;
+    }
+
+    if (typeof defaultMember !== 'undefined') {
+      this.member = defaultMember;
+    }
+
+    fetch("./member.json").then(function (resp) {
+      return resp.json();
+    }).then(function (json) {
+      _this.member = json;
+
+      if ('name' in _this.member) {
+        document.title = `${_this.member.name} | 乃木坂46卒業メンバーのブログ`;
+      }
+    }).catch(function (err) {
+      console.log(err);
+    });
+    fetch("./result.json").then(function (resp) {
+      return resp.json();
+    }).then(function (json) {
+      _this.isLoading = false;
+      _this.list = json;
+      var no = new URL(window.location).searchParams.get("no");
+
+      if (no != null && no != '') {
+        _this.selected = new Number(no);
+        Vue.nextTick().then(function () {
+          //第一次開啟時將卷軸捲到正確的位置
+          var dom = document.querySelector(".menu li:first-of-type");
+          var liHeight = dom.scrollHeight;
+          document.querySelector(".menu").scrollTop = liHeight * _this.selected;
+        });
+      }
+    }).catch(function (err) {
+      _this.isLoading = false;
+      console.log(err);
+    });
+  }
 });
