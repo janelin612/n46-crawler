46d1adf1c148a149e24547527cf942f3060c7a37
janelin612 <wgl1242254@gmail.com>
2019-06-18 10:05:43 +0800

    導入promise來處理渲染畫面與API呼叫的時機先後問題

--- viewer/script/main.js
+++ viewer/script/main.js
@@ -1,38 +1,37 @@
 var app = new Vue({
     el: "#app",
     data: {
-        weekday:['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
-        firstTimeUpdate:true,
+        weekday: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
         selected: 0,
         list: [],
         member: {},
-        isFold:false
+        isFold: false
     },
-    computed:{
+    computed: {
         //指向目前選定的部落格文章
-        currentPost:function(){
-            if(this.list.length==0 || this.selected<0 ||this.selected>=this.list.length ){
+        currentPost: function () {
+            if (this.list.length == 0 || this.selected < 0 || this.selected >= this.list.length) {
                 return null;
-            }else{
+            } else {
                 return this.list[this.selected];
             }
         },
         //由date計算出來實際要顯示的日期樣式物件
-        currentPostDate:function(){
-            if(this.currentPost==null){
+        currentPostDate: function () {
+            if (this.currentPost == null) {
                 return {
-                    yearMonth:"",
-                    date:"",
-                    weekday:""
+                    yearMonth: "",
+                    date: "",
+                    weekday: ""
                 }
-            }else{
+            } else {
                 var time = new Date(this.currentPost.datetime);
-                var month=(time.getMonth()+1)>9?""+(time.getMonth()+1):"0"+(time.getMonth()+1);
-                var date=time.getDate()>9?""+time.getDate():"0"+time.getDate();
+                var month = (time.getMonth() + 1) > 9 ? "" + (time.getMonth() + 1) : "0" + (time.getMonth() + 1);
+                var date = time.getDate() > 9 ? "" + time.getDate() : "0" + time.getDate();
                 return {
-                    yearMonth:time.getFullYear()+"/"+month,
-                    date:date,
-                    weekday:this.weekday[time.getDay()]
+                    yearMonth: time.getFullYear() + "/" + month,
+                    date: date,
+                    weekday: this.weekday[time.getDay()]
                 }
             }
         }
@@ -40,39 +39,46 @@ var app = new Vue({
     methods: {
         onListSelected: function (index) {
             window.scrollTo(0, 0);
-            this.isFold=true;
+            this.isFold = true;
             this.selected = index;
             var obj = { Url: window.location.toString().split("?")[0] + "?no=" + index };
             history.replaceState(obj, document.title, obj.Url);
         },
-        onToggle:function(){
-            this.isFold=!this.isFold;
+        onToggle: function () {
+            this.isFold = !this.isFold;
         }
     },
     created: function () {
-        this.$http.get("./result.json").then(res => { this.list = res.body }, res => { this.list = [] });
-        this.$http.get("./member.json").then(res => { this.member = res.body }, res => { this.member = {} });
-
-        var no = new URL(window.location).searchParams.get("no");
-        if (no != null && no != '') {
-            this.selected = no;
-        }
+        fetch("./member.json")
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((json) => {
+                this.member = json;
+                if ('name' in this.member) {
+                    document.title = this.member.name + " 非公式ブログ";
+                }
+                return fetch("./result.json");
+            })
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((json) => {
+                this.list = json;
+                let no = new URL(window.location).searchParams.get("no");
+                if (no != null && no != '') {
+                    this.selected = new Number(no);
+                    Vue.nextTick()
+                        .then(() => {
+                            //第一次開啟時將卷軸捲到正確的位置
+                            let dom = document.querySelector(".menu li:first-of-type");
+                            let liHeight = dom.scrollHeight;
+                            document.querySelector(".menu").scrollTop = liHeight * (this.selected);
+                        })
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     },
-    updated:function(){
-        
-        if(this.firstTimeUpdate){
-            //更改網站標題
-            if('name' in this.member){
-                document.title=this.member.name+" 非公式ブログ";
-            }
-
-            //第一次開啟時將卷軸捲到正確的位置
-            var dom=this.$el.querySelector(".menu li");
-            if(dom!=null){
-                var single=dom.scrollHeight;
-                this.$el.querySelector(".menu").scrollTop=single*(this.selected);
-                this.firstTimeUpdate=false;
-            }
-        }
-    }
 });
