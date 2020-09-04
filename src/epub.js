70242d4246815dcf3b91715097d163e1f9eb8220
janelin612 <wgl1242254@gmail.com>
2020-09-04 11:55:04 +0800

    try to generate epub

---
+++ src/epub.js
@@ -0,0 +1,24 @@
+/**
+ *  Convert archived blog in ./viewer into epub 
+ *  For downloading image,you should run a static server at 127.0.0.1:8080 before run this code.
+ */
+const Epub = require("epub-gen");
+const Fs = require("fs");
+
+let member = JSON.parse(Fs.readFileSync("./viewer/member.json"));
+let content = JSON.parse(Fs.readFileSync("./viewer/result.json"));
+
+const option = {
+  title: member.name,
+  author: member.name,
+  tocTitle: "目次",
+  content: content.map((value) => {
+    return {
+      title: value.title,
+      author: value.author,
+      data: value.content.replace(/\"img\//g, "\"http://127.0.0.1:8080/img/")
+    }
+  })
+};
+
+new Epub(option, "./viewer/blog.epub");
