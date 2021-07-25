e0f835cfb1dcfdf60ed1f072ce38580dfa05ccc8
janelin612 <wgl1242254@gmail.com>
2021-07-25 23:41:09 +0800

    整理程式碼

--- crawler/viewer/script/main.js
+++ crawler/viewer/script/main.js
@@ -1,5 +1,5 @@
 var app = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
     weekday: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
     selected: 0,
@@ -10,30 +10,34 @@ var app = new Vue({
   },
   computed: {
     //指向目前選定的部落格文章
-    currentPost: function () {
-      if (this.list.length == 0 || this.selected < 0 || this.selected >= this.list.length) {
+    currentPost() {
+      if (
+        this.list.length == 0 ||
+        this.selected < 0 ||
+        this.selected >= this.list.length
+      ) {
         return null;
       } else {
         return this.list[this.selected];
       }
     },
     //由date計算出來實際要顯示的日期樣式物件
-    currentPostDate: function () {
+    currentPostDate() {
       if (this.currentPost == null) {
         return {
-          yearMonth: "",
-          date: "",
-          weekday: ""
-        }
+          yearMonth: '',
+          date: '',
+          weekday: ''
+        };
       } else {
         let time = new Date(this.currentPost.datetime);
         let month = this.formatNumber(time.getMonth() + 1);
         let date = this.formatNumber(time.getDate());
         return {
-          yearMonth: time.getFullYear() + "/" + month,
+          yearMonth: time.getFullYear() + '/' + month,
           date: date,
           weekday: this.weekday[time.getDay()]
-        }
+        };
       }
     }
   },
@@ -42,7 +46,12 @@ var app = new Vue({
       window.scrollTo(0, 0);
       this.isFold = true;
       this.selected = index;
-      var obj = { Url: window.location.toString().split("?")[0] + "?no=" + index };
+      let url = window.location.origin + window.location.pathname;
+      let param = new URLSearchParams();
+      param.set('no', index);
+      var obj = {
+        Url: `${url}?${param.toString()}`
+      };
       history.replaceState(obj, document.title, obj.Url);
     },
     onToggle: function () {
@@ -53,45 +62,31 @@ var app = new Vue({
       return num > 9 ? `${num}` : `0${num}`;
     }
   },
-  created: function () {
-    if (typeof defaultList !== 'undefined') {
-      this.list = defaultList;
-    }
-    if (typeof defaultMember !== 'undefined') {
-      this.member = defaultMember;
+  async created() {
+    try {
+      this.member = await (await fetch('./member.json')).json();
+      if ('name' in this.member) {
+        document.title = `${this.member.name} | 乃木坂46卒業メンバーのブログ`;
+      }
+    } catch (error) {
+      console.warn(error);
     }
 
-    fetch("./member.json")
-      .then(resp => resp.json())
-      .then((json) => {
-        this.member = json;
-        if ('name' in this.member) {
-          document.title = `${this.member.name} | 乃木坂46卒業メンバーのブログ`;
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    fetch("./result.json")
-      .then(resp => resp.json())
-      .then((json) => {
-        this.isLoading = false;
-        this.list = json;
-        let no = new URL(window.location).searchParams.get("no");
-        if (no != null && no != '') {
-          this.selected = new Number(no);
-          Vue.nextTick()
-            .then(() => {
-              //第一次開啟時將卷軸捲到正確的位置
-              let dom = document.querySelector(".menu li:first-of-type");
-              let liHeight = dom.scrollHeight;
-              document.querySelector(".menu").scrollTop = liHeight * (this.selected);
-            })
-        }
-      })
-      .catch((err) => {
-        this.isLoading = false;
-        console.log(err);
-      });
-  },
+    try {
+      this.list = await (await fetch('./result.json')).json();
+      this.isLoading = false;
+      let no = new URL(window.location).searchParams.get('no');
+      if (no != null && no != '') {
+        this.selected = new Number(no);
+        await Vue.nextTick();
+        //第一次開啟時將卷軸捲到正確的位置
+        let dom = document.querySelector('.menu li:first-of-type');
+        let liHeight = dom.scrollHeight;
+        document.querySelector('.menu').scrollTop = liHeight * this.selected;
+      }
+    } catch (error) {
+      console.warn(error);
+      this.isLoading = false;
+    }
+  }
 });
