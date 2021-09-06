962c248e1005a89a584331896d36c53c4342d414
janelin612 <janelin612@gmail.com>
2021-09-06 22:56:37 +0800

    Update to 2021/09

--- _nuxt/static/1630496596/mb/kotoko.sasaki/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fn46-crawler\u002F_nuxt\u002Fstatic\u002F1630496596",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmb\u002Fkotoko.sasaki",config:{_app:{basePath:"\u002Fn46-crawler\u002F",assetsPath:"\u002Fn46-crawler\u002F_nuxt\u002F",cdnURL:a}}}}(null));
