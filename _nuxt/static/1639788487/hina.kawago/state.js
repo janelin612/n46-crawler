edd817ade0efe1f22417bd1985f6442890826e72
janelin612 <janelin612@gmail.com>
2022-01-06 19:19:44 +0800

    Update to 2022/01

--- _nuxt/static/1639788487/hina.kawago/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fn46-crawler\u002F_nuxt\u002Fstatic\u002F1639788487",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhina.kawago",config:{_app:{basePath:"\u002Fn46-crawler\u002F",assetsPath:"\u002Fn46-crawler\u002F_nuxt\u002F",cdnURL:a}}}}(null));
