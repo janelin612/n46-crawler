127edf02cfd8c7eed5cb28d017f443b332afb4ba
janelin612 <wgl1242254@gmail.com>
2022-02-17 14:17:47 +0800

    大致更新完成爬蟲架構

--- crawler/src/n46-crawler.js
+++ crawler/src/n46-crawler.js
@@ -7,7 +7,7 @@ const MEMBER_PAGE = DOMAIN + '/s/n46/artist';
 const BLOG_PAGE = DOMAIN + '/s/n46/diary/MEMBER';
 
 /** Demo Site 的目錄位置 */
-const PROJECT_FOLDER = '../../n46-crawler/mb/';
+const PROJECT_FOLDER = '../../n46-crawler/mb';
 const RESULT_JSON_FILE = 'result.json';
 const MEMBER_INFO_FILE = 'member.json';
 
@@ -17,12 +17,13 @@ module.exports = {
   listMember() {
     memberListCrawler.queue(BLOG_PAGE);
   },
-  download(memberPath, save) {
+  download(ct, save) {
     if (save) {
-      createDirectory(memberPath);
+      createDirectory(ct);
     }
     ImageUtil.setDirectory(folder);
-    archiveListCrawler.queue(BLOG_URL + memberPath);
+    memberInfoCrawler.queue(`${MEMBER_PAGE}/${ct}`);
+    pageCursor.queue(`${BLOG_PAGE}/list?ct=${ct}&page=0`);
   }
 };
 
@@ -31,7 +32,7 @@ module.exports = {
  */
 function createDirectory(memberPath) {
   if (fs.existsSync(PROJECT_FOLDER)) {
-    folder = `${PROJECT_FOLDER}${memberPath}/`;
+    folder = `${PROJECT_FOLDER}/${memberPath}/`;
     if (!fs.existsSync(folder)) {
       fs.mkdirSync(folder);
     }
@@ -61,22 +62,42 @@ let memberListCrawler = new Crawler({
   }
 });
 
+let pageCursor = new Crawler({
+  maxConnections: 1,
+  callback(error, res, done) {
+    if (error) {
+      console.warn(error);
+    } else {
+      let $ = res.$;
+      //拉每一則的清單
+      $('main div.bl--list a').each((index, value) => {
+        let url = DOMAIN + $(value).attr('href');
+        contentCrawler.queue(url);
+      });
+      //找下一頁
+      $('main div.bl--pg ul li').each((index, value) => {
+        value = $(value);
+        if (value.find('span.active').length > 0) {
+          let next = value.next();
+          if (next.find('a').length > 0) {
+            let url = DOMAIN + next.children('a').attr('href');
+            pageCursor.queue(url);
+          }
+        }
+      });
+    }
+    done();
+  }
+});
+
 /**
  * 儲存最後結果
  */
 var result = [];
-/**
- * 下載內文資訊
- */
-let blogContentCrawler = new Crawler({
-  maxConnections: 5,
-  jQuery: {
-    name: 'cheerio',
-    options: {
-      decodeEntities: false
-    }
-  },
-  callback: (error, res, done) => {
+let contentCrawler = new Crawler({
+  maxConnections: 10,
+  jQuery: { name: 'cheerio', options: { decodeEntities: false } },
+  callback(error, res, done) {
     if (error) {
       console.warn(error);
     } else {
@@ -86,7 +107,7 @@ let blogContentCrawler = new Crawler({
       const REGEX_MATCH_IMG_URL =
         /https{0,1}:\/\/\S+?\.nogizaka46\.com\/\S+\.(jpg|jpeg|png|gif)/;
 
-      $('#sheet div.entrybody a').each((index, value) => {
+      $('main div.bd--edit a').each((index, value) => {
         let href = $(value).attr('href');
         // 拔掉DecoMailer的圖片備份連結
         if (href && href.indexOf('dcimg.awalker.jp') != -1) {
@@ -104,40 +125,46 @@ let blogContentCrawler = new Crawler({
         }
       });
 
-      // 將圖片網址改為本地端位置，並下載圖片
-      $('#sheet div.entrybody img').each(function (index, value) {
-        let src = $(value).attr('src');
-        if (src && src.match(REGEX_MATCH_IMG_URL)) {
-          let localPath = ImageUtil.download(src);
-          $(value).attr('src', localPath);
+      $('main div.bd--edit img').each((index, value) => {
+        value = $(value);
+        let src = value.attr('src');
+        if (src && !src.startsWith('blob')) {
+          let localPath;
+          if (src.startsWith('/')) {
+            localPath = ImageUtil.download(DOMAIN + src);
+          } else {
+            localPath = ImageUtil.download(src);
+          }
+          value.attr('src', localPath);
         }
       });
 
+      let title = $('header h1.bd--hd__ttl').text().trim();
+      let datetime = $('header div.bd--hd__sub p.bd--hd__date').text().trim();
+      let content = $('main div.bd--edit').html();
+      let url = new URL(res.request.uri.href);
+      url = url.origin + url.pathname;
+
       let item = {
-        datetime: $('#sheet div.entrybottom').text().trim(),
-        author: $('#sheet h1.clearfix .heading .author').text(),
-        title: $('#sheet h1.clearfix .heading .entrytitle').html(),
-        url: res.request.uri.href,
-        content: $('#sheet div.entrybody').html()
+        datetime,
+        title,
+        content,
+        url
       };
+      console.log(datetime + ' | ' + title);
       result.push(item);
-      console.log(item.datetime + ' | ' + item.title);
     }
     done();
   }
 });
-
-/**
- * 啟用多線程下載的話最後需要重新排序
- */
-blogContentCrawler.on('drain', function () {
-  const regex = /20[0-9]{2}\/[0-9]{2}\/[0-9]{6}/;
+contentCrawler.on('drain', () => {
+  const regex = /\/detail\/(\d+)/;
   result.sort((a, b) => {
-    let idB = b.url.match(regex)[0].replace(/\//g, '');
-    let idA = a.url.match(regex)[0].replace(/\//g, '');
+    let idA = a.url.match(regex)[1];
+    let idB = b.url.match(regex)[1];
     return parseInt(idB) - parseInt(idA);
   });
-  fs.writeFileSync(folder + RESULT_JSON_FILE, JSON.stringify(result), 'utf8');
+  fs.writeFileSync(folder + RESULT_JSON_FILE, JSON.stringify(result), 'utf-8');
 });
 
 /**
@@ -151,45 +178,37 @@ let memberInfoCrawler = new Crawler({
     } else {
       let $ = res.$;
 
-      //處理成員頭像
-      let memberImage = $('#profile img').first().attr('src');
-      if (memberImage) {
-        memberImage = ImageUtil.download(memberImage);
-      }
-
       //成員姓名
-      let memberName_hiragana = $('#profile div.txt h2 span').text();
-      $('#profile div.txt h2 span').remove();
-      let memberName = $('#profile div.txt h2').text();
+      let name_hiragana = $('header div.md--hd__name p.md--hd__j.f--head')
+        .text()
+        .trim();
+      let name = $('header h1.f--head').text().trim();
+
+      //照片
+      let img = $('main div.md--hd__fig>div').data('src');
+      img = ImageUtil.download(DOMAIN + img);
 
       //table
       let introList = [];
-      $('#profile div.txt dl dt').each((index, item) => {
-        let key = $(item).text().replace('：', '');
-        let value = $(item).nextAll('dd').first().text();
+      $('div.md--hd__ds dl').each((index, item) => {
+        let key = $(item).children('dt').text().trim();
+        let value = $(item).children('dd').text().trim();
         introList.push({
-          key: key,
-          value: value
+          key,
+          value
         });
       });
 
-      //tag
-      let tagList = [];
-      $('#profile div.txt div.status div').each((index, item) => {
-        tagList.push($(item).text());
-      });
-
-      //write all
-      let member = {
-        name: memberName,
-        name_hiragana: memberName_hiragana,
-        image: memberImage,
+      let output = {
+        name,
+        name_hiragana,
         intro: introList,
-        tag: tagList
+        image: img,
+        tag: []
       };
       fs.writeFileSync(
         folder + MEMBER_INFO_FILE,
-        JSON.stringify(member),
+        JSON.stringify(output),
         'utf8'
       );
     }
