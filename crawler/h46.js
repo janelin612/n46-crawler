25cb50a5551526287bd3344acc3507fab537666c
iamcristye@outlook.com <iamcristye@outlook.com>
2022-06-03 18:46:31 +0800

    Hinatazaka crawler added

---
+++ crawler/h46.js
@@ -0,0 +1,9 @@
+const crawler = require('./src/h46-crawler');
+//自command line帶入參數
+var argv = require('minimist')(process.argv.slice(2));
+
+if (argv.a) {
+    crawler.download(argv.a);
+} else {
+    crawler.listMember();
+}
