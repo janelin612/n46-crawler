67e185ec7d8cbef258703e5e03677261fc84a171
janelin612 <janelin612@gmail.com>
2022-11-02 21:13:58 +0800

    update to 2022/10

--- _nuxt/static/1658228829/mb/kazumi.takayama/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fn46-crawler\u002F_nuxt\u002Fstatic\u002F1658228829",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmb\u002Fkazumi.takayama",config:{_app:{basePath:"\u002Fn46-crawler\u002F",assetsPath:"\u002Fn46-crawler\u002F_nuxt\u002F",cdnURL:a}}}}(null));
