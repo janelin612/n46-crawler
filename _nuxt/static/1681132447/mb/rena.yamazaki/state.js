83748e715f244ee0ab059dab444043c4e4a69042
janelin612 <janelin612@gmail.com>
2023-04-10 23:09:03 +0800

    Update to 2023.04

---
+++ _nuxt/static/1681132447/mb/rena.yamazaki/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fn46-crawler\u002F_nuxt\u002Fstatic\u002F1681132447",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmb\u002Frena.yamazaki",config:{_app:{basePath:"\u002Fn46-crawler\u002F",assetsPath:"\u002Fn46-crawler\u002F_nuxt\u002F",cdnURL:a}}}}(null));
